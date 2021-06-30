cc3a4e091e84bd7b8e02726aac3736ff6776acf3
ismailhssini <86742132+ismailhssini@users.noreply.github.com>
2021-06-30 21:58:34 +0200

    Update iterative_fibo.c

--- TP1/EX1/iterative_fibo.c
+++ TP1/EX1/iterative_fibo.c
@@ -2,5 +2,16 @@
 
 int iterative_fibo(int n)
 {
-    // write your iterative_fibo code here
+    int nbr1=0,nbr2=1,suivant;
+	if (n==0)
+		return 0;
+	if (n==1)
+		return  1;
+	for (int i = 2; i < n+1; i++)
+  	{
+      		suivant = nbr1 + nbr2;
+      		nbr1 = nbr2;
+      		nbr2 = suivant;
+    }	
+ 	return suivant;
 }
